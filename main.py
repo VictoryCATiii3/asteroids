754ff9fc21d5363a5100115f5d807105177c61f3
VictoryCATiii3 <victor7670236@gmail.com>
2025-01-19 11:41:28 -0800

    Drawing the players ship in the center of the screen

--- main.py
+++ main.py
@@ -2,20 +2,31 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import *
 from constants import *
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        pygame.display.update()
+
+        player.draw(screen)
+
+        pygame.display.flip()
+        dt += clock.tick()
 
 if __name__ == "__main__":
     main()
